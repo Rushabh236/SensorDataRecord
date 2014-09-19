ef00bdd142fe8430d693c04dc0d912195bd17e2b
Rushabh <rushabhshah230691@gmail.com>
2014-09-19 18:03:56 +0200

    Made a file on device but it doesn'T write the data.

--- SensorDataRecording/src/com/Rushabh/sensordatarecording/MainActivity.java
+++ SensorDataRecording/src/com/Rushabh/sensordatarecording/MainActivity.java
@@ -6,32 +6,24 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
-//import java.security.Timestamp;
 import java.text.DecimalFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
-import android.support.v7.app.ActionBarActivity;
+
+
+//import android.support.v7.app.ActionBarActivity;
 import android.text.format.DateFormat;
 import android.app.Activity;
-//import android.os.AsyncTask;
 import android.os.Bundle;
-//import android.os.Environment;
-//import android.os.StrictMode;
-//import android.util.Log;
+import android.os.Environment;
 import android.view.Menu;
-//import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
-
-//import org.openintents.sensorsimulator.hardware.Sensor;
-//import org.openintents.sensorsimulator.hardware.SensorEvent;
-//import org.openintents.sensorsimulator.hardware.SensorEventListener;
-//import org.openintents.sensorsimulator.hardware.SensorManagerSimulator;
-
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -43,6 +35,7 @@ public class MainActivity extends Activity implements SensorEventListener {
     EditText textData;
     Button startButton;
     Button stopButton;
+    TextView textview;
 
     File myFile;
     FileOutputStream fOut;
@@ -52,15 +45,13 @@ public class MainActivity extends Activity implements SensorEventListener {
 
 
     private SensorManager sensorManager;
-    //private ConnectionToSensorSimulator conn;
     private long currentTime;
     private long startTime;
     private long x = 0;
     private long y = 0;
-    //Timestamp timeNow;
 
     float[] acceleration = new float[3];
-    float[] magneticField = new float[3];
+    //float[] magneticField = new float[3];
 
     boolean stopFlag = false;
     boolean startFlag = false;
@@ -82,12 +73,18 @@ public class MainActivity extends Activity implements SensorEventListener {
             public void onClick(View v) {
                 // start recording the sensor data
                 try {
-                	File iSdCard = getFilesDir();
                 	
-                	//File sdcard = Environment.getExternalStorageDirectory();
-                	myFile = new File(iSdCard, textData.getText()+ ".txt");
+                	String root = Environment.getExternalStorageDirectory().toString();
+                	textview = (TextView) findViewById(R.id.textView);
+                	//String root = getFilesDir().toString();
+                	textview.setText(root);
                 	
-                	//myFile = new File("/sdcard/" + textData.getText() + ".txt");
+                	//File iSdCard = getFilesDir();
+                	
+                	//File sdcard = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getPath()+ "/SensorTest/");
+                	File sdcard = Environment.getExternalStorageDirectory();
+                	myFile = new File(sdcard, textData.getText()+ ".txt");
+                
                     myFile.createNewFile();
 
                     fOut = new FileOutputStream(myFile);
@@ -122,15 +119,12 @@ public class MainActivity extends Activity implements SensorEventListener {
         //StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
 		//StrictMode.setThreadPolicy(policy);
 		sensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-		//sensorManager.connectSimulator();
 		
 		
 		 // register this class as a listener for the sensors
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
-		
-		//conn = new ConnectionToSensorSimulator();
-	    //conn.execute();
+        //sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD), SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
@@ -158,23 +152,19 @@ public class MainActivity extends Activity implements SensorEventListener {
                     acceleration[2] = event.values[2];
                     
     	        break;
+    	        /**
     	        case Sensor.TYPE_MAGNETIC_FIELD:
     	        	magneticField[0] = event.values[0];
                     magneticField[1] = event.values[1];
                     magneticField[2] = event.values[2];
     	 
     	        break;
-    	        
+    	        */
     	        default:
     	        break;
     	        }
     	    }
         	/**
-        	if (event.type() == Sensor.TYPE_ACCELEROMETER) {
-                acceleration[0] = event.values[0];
-                acceleration[1] = event.values[1];
-                acceleration[2] = event.values[2];
-            }
 
             if (event.Type() == Sensor.TYPE_MAGNETIC_FIELD) {
                 magneticField[0] = event.values[0];
@@ -182,10 +172,6 @@ public class MainActivity extends Activity implements SensorEventListener {
                 magneticField[2] = event.values[2];
             }
 
-            if (isFirstSet) {
-                startTime = System.currentTimeMillis();
-                isFirstSet = false;
-            }
             */
 
             
@@ -277,56 +263,4 @@ public class MainActivity extends Activity implements SensorEventListener {
 
     }
     
-}    
-/**    private class ConnectionToSensorSimulator extends
-    AsyncTask<Void, Void, Boolean> {
-
-@Override
-protected Boolean doInBackground(Void... params) {
-    Log.d("SENSOR", "CONNECTING TO SENSOR SIMULATOR");
-    sensorManager.connectSimulator();
-    return true;
 }
-
-@Override
-protected void onPostExecute(Boolean result) {
-    super.onPostExecute(result);
-    if (result) {
-    Log.d("SENSOR", "CONNECTED TO SENSOR SIMULATOR");
-    } else {
-    Log.d("SENSOR", "NOT CONNECTED TO SENSOR SIMULATOR");
-    }
-}
-
-}
-}
-*/
-
-/**public class MainActivity extends ActionBarActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-}*/
